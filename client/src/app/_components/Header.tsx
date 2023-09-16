3208a62eb4686bae2f804ccbb8530d39911ebec7
unknown <alexvera0109@gmail.com>
2023-09-16 08:24:05 -0400

    halfway quiz page

--- client/src/app/_components/Header.tsx
+++ client/src/app/_components/Header.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react'
 import { HiMenu } from 'react-icons/hi'
 const Header = () => {
   return (
-    <div className='sticky py-5 top-5 bg-[#7ad87d] rounded-lg px-5 flex w-10/12 justify-between mt-5 items-center z-10 backdrop-filter backdrop-blur bg-opacity-80'>
+    <div className='sticky py-5 top-5 bg-[#7ad87d] rounded-lg px-5 flex w-full justify-between mt-5 items-center z-10 backdrop-filter backdrop-blur bg-opacity-80'>
       <Link className='flex items-center gap-1 text-xl' href={'/'}>
         <h2 className='text-2xl'>CreditCardApp</h2>
       </Link>
