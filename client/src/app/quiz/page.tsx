f3a78519b228b3906da9f19061da1e53fc1c0a07
unknown <alexvera0109@gmail.com>
2023-09-16 06:20:13 -0400

    init quiz page

--- client/src/app/quiz/page.tsx
+++ client/src/app/quiz/page.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <h1>QUIZ PAGE</h1>
+    <div className='w-full h-screen flex flex-col items-center'>
+      <p>FIND YOUR PERFECT CREDIT CARD</p>
+      <h1 className='text-4xl'>The Card Quiz</h1>
+      <p>
+        Welcome to APPNAME, where finding the perfect credit card is effortless. Take our quick quiz, to match your financial needs and goals in
+        seconds.
+      </p>
     </div>
   )
 }
