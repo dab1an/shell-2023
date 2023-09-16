41a52a20aa80724c919a3df6fd510de2b6efec3f
dab1an <dabianextras@gmail.com>
2023-09-16 09:47:26 -0400

    question order

--- client/src/app/quiz/[problem]/page.tsx
+++ client/src/app/quiz/[problem]/page.tsx
@@ -1,56 +1,83 @@
-import React from 'react'
+import { Chonburi } from "next/font/google";
+import React from "react";
 type QuizData = {
   [key: string]: {
-    question: string
-    choices: string[]
-  }
-}
+    question: string;
+    choices: string[];
+  };
+};
 const Page = ({ params }: any) => {
   const quizData: QuizData = {
     problem1: {
-      question: 'What is your credit score range?',
-      choices: ['Excellent (above 750)', 'Good (700-749)', 'Fair (650-699)', 'Poor (below 650)'],
+      question: "Are you currently a student?",
+      choices: ["Yes", "No"],
     },
+
     problem2: {
-      question: 'What is your main reason for getting a credit card?',
-      choices: ['Building credit', 'Earning rewards', 'Emergency expenses', 'Travel benefits'],
+      question: "What is your main reason for getting a credit card?",
+      choices: [
+        "Building credit",
+        "Earning rewards",
+        "Emergency expenses",
+        "Travel benefits",
+      ],
     },
+
     problem3: {
-      question: 'How often do you plan to use the credit card for purchases?',
-      choices: ['Daily', 'Weekly', 'Monthly', 'Rarely'],
+      question: "What is your credit score range?",
+      choices: [
+        "Excellent (above 750)",
+        "Good (700-749)",
+        "Fair (650-699)",
+        "Poor (below 650)",
+      ],
     },
     problem4: {
-      question: 'Do you plan to carry a balance on your credit card month-to-month?',
-      choices: ['Yes, often', 'Occasionally', 'No, I pay in full each month'],
+      question: "How often do you plan to use the credit card for purchases?",
+      choices: ["Daily", "Weekly", "Monthly", "Rarely"],
     },
     problem5: {
-      question: 'Are you interested in earning cashback rewards?',
-      choices: ['Yes', 'No'],
+      question: "Are you interested in earning cashback rewards?",
+      choices: ["Yes", "No"],
     },
     problem6: {
-      question: 'Do you travel frequently and want travel rewards or benefits?',
-      choices: ['Yes', 'No'],
+      question: "Do you travel frequently and want travel rewards or benefits?",
+      choices: ["Yes", "No"],
     },
     problem7: {
-      question: 'Are you willing to pay an annual fee for premium benefits?',
-      choices: ['Yes', 'No'],
+      question: "Are you willing to pay an annual fee for premium benefits?",
+      choices: ["Yes", "No"],
     },
     problem8: {
-      question: 'Have you had any credit card debt in the past?',
-      choices: ['Yes', 'No'],
+      question: "Have you had any credit card debt in the past?",
+      choices: ["Yes", "No"],
     },
     problem9: {
-      question: 'What is your estimated monthly income?',
-      choices: ['Less than $1,000', '$1,000 - $2,500', '$2,500 - $5,000', 'More than $5,000'],
+      question: "Have you had any credit card debt in the past?",
+      choices: ["Yes", "No"],
     },
-  }
-  const data = quizData[params.problem]
+    problem10: {
+      question: "Do you have a bank preference",
+      choices: [
+        "Chase",
+        "Wells Fargo",
+        "TD Bank",
+        "Bank of America",
+        "Capital One",
+        "American Express",
+        "Discover",
+        "Citi Bank",
+        "No preference",
+      ],
+    },
+  };
+  const data = quizData[params.problem];
   return (
     <div>
       <h1>{params.problem}</h1>
       <p>{data.question}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
