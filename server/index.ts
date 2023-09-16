00b4ccd5fb3aad98ef8c0ce42855fc3654f73ded
unknown <alexvera0109@gmail.com>
2023-09-16 05:04:18 -0400

    test

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,15 @@
-const express = require('express')
-const dotenv = require('dotenv')
+import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express'
 
 dotenv.config()
 
-const app = express()
+const app: Express = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
 })
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
 })
