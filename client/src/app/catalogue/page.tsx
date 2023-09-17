af5d2a9f36c3ad70421684a075f4ec70d728eb4a
dab1an <dabianextras@gmail.com>
2023-09-16 22:53:36 -0400

    Catalogue skeleton

--- client/src/app/catalogue/page.tsx
+++ client/src/app/catalogue/page.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-
+import ResultCard from "../_components/ResultCard";
+import cardInfo from "./cardInfo";
 const page = () => {
   return (
-    <div className="bg-[#fffff0] flex ">
-      <div></div>
+    <div className="bg-[#fffff0] flex items-center ">
+      <div className="flex flex-col">
+        <h1>Catalogue</h1>
+        <div className="flex flex-wrap gap-5 justify-center p-4">
+          {cardInfo.map((e, i) => {
+            return (
+              <ResultCard
+                key={i}
+                dataName={e.name}
+                dataDesc={""}
+                cardImg={e.url}
+                main={false}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
