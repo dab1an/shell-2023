0e8a4cc976fb23571f3f36ccd7b7c50d744e521b
02danieljonas <02danieljonas@gmail.com>
2023-09-17 10:40:20 -0400

    changed style

--- client/src/app/_components/ResultCard.tsx
+++ client/src/app/_components/ResultCard.tsx
@@ -21,17 +21,17 @@ const ResultCard = (props: resultCardProps) => {
       className={`relative flex w-80 flex-col ${
         props.main ? "shadow-2xl" : ""
       } items-center rounded-xl border-2 border-neutral-700 bg-white bg-clip-border text-gray-700 shadow-md mt-16 ${
-        props.main ? "mb-16" : "justify-end"
+        props.main ? "mb-16" : ""
       }`}
     >
-      <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600 border border-gray-500">
+      <div className="absolute top-0 mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600 border border-gray-500">
         {cardImg && <img src={cardImg} alt="" className="w-full h-full" />}
       </div>
-      <div className="p-6">
+      <div className="px-6 pb-6 pt-40">
         <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased min-h-[60px]">
           {props.dataName}
         </h5>
-        <p className="block font-sans font-light leading-relaxed text-inherit antialiased text-sm">
+        <p className="block overflow-auto font-sans font-light h-[250px] leading-relaxed text-inherit antialiased text-sm">
           {props.dataDesc}
         </p>
       </div>
--- client/src/app/_components/TheButton.tsx
+++ client/src/app/_components/TheButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TheButton = ({ children, secondary }: any) => {
   return (
-    <button className={`buttonTB ${secondary && 'buttonLightTB'}`}>
+    <button disabled className={`hover:cursor-not-allowed buttonTB ${secondary && 'buttonLightTB'}`}>
       <span>{children}</span>
       <svg width='34' height='34' viewBox='0 0 74 74' fill='none' xmlns='http://www.w3.org/2000/svg'>
         <circle cx='37' cy='37' r='35.5' stroke='black' stroke-width='3'></circle>
